9a41e203394937714951ac86f2df020c7a866677
Codesamp-Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2024-06-27 20:46:26 +0530

    added some more boxes in home-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,68 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import { isMobile } from "react-device-detect";
-// import profileImg from "./assets/Rohan.jpg";
-// import account from "./assets/account.png";
 import MouseFollower from "./components/mouseFollower";
 import TechMarquee from "./components/infiniteScroller";
 import HomeCard from "./components/homeCard";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const triggerPoint = 100; // Adjust this value to control when to hide
+
+      if (scrollY > triggerPoint) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
       {!isMobile && <MouseFollower />}
-      <nav className="section--1 flex justify-between items-center p-[2rem]">
+      <nav className="section--1 flex z-[900] justify-between items-center p-[2rem]">
         <p>Portfolio.io</p>
         <button className="bg-[#eee] px-3.5 py-[4px] border-[0.5px] border-[#bbb] rounded-lg">
           menu
         </button>
       </nav>
-      <div className="w-full h-[85vh] p-[2rem] relative">
-        <HomeCard />
+      <div className="home--section w-full h-[85vh] p-[2rem] relative overflow-hidden">
+        <div
+          className={`spotify--section z-[999] absolute flex items-center w-fit bg-[#e1ffec] ring-1 ring-[#00D960] gap-[1rem] rotate-[3deg] md:rotate-[-9deg] duration-700 rounded-full p-2 ${
+            !isVisible ? "opacity-0" : ""
+          }`}>
+          <span className="flex items-center gap-[10px]">
+            <img
+              className="spotify--icon w-[24px] md:w-[40px] rounded-lg"
+              src="./spotify.png"></img>
+            <span>
+              <h2 className="font-bold text-[12px] md:text-[16px]">
+                Stay with me
+              </h2>
+              <p className="text-[8px] md:text-[12px] text-[#777]">
+                Miki Matsubara
+              </p>
+            </span>
+          </span>
+          <button className="bg-[#00D960] text-white text-[14px] md:text-[18px] font-bold rounded-full py-1 px-4">
+            Play
+          </button>
+        </div>
+        <HomeCard isVisible={isVisible} />
         <h1
           data-value="my name. "
-          className="leading-none pb-4 md:leading-[20vh] text-[4rem] md:text-[10rem] text-left md:text-center font-medium absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-70%] z-[900]"
+          className="leading-none pb-4 md:leading-[10vh] text-[3rem] md:text-[6rem] text-left md:text-center font-medium absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-70%] z-[900]"
           style={{
             background:
-              "linear-gradient(to right, #f32170, #ff6b08,#cf23cf, #eedd44)",
+              "linear-gradient(to right, #f32170, #ff6b08,#cf23cf, #FFDD04)",
             WebkitTextFillColor: "transparent",
             WebkitBackgroundClip: "text",
             backgroundClip: "text", // Standard property for wider compatibility
@@ -35,11 +74,14 @@ function App() {
       </div>
       <div
         data-value="about me."
-        className="mx-[2rem] mb-[2rem] md:m-[2rem] md:w-[60%] bg-[#f7f7f7] md:bg-[#fff] rounded-2xl p-[2rem] md:p-0 border-[1px] border-[#bbb] md:border-0">
+        className="mx-[2rem] mb-[2rem] md:m-[2rem] bg-[#f7f7f7] md:bg-[#fff] rounded-2xl p-[2rem] md:p-0 border-[1px] border-[#bbb] md:border-0 relative">
+        <img
+          className="snap-img absolute z-[-1] md:z-[100] rotate-[0deg] top-[-100px] sm:top-[-140px] md:top-[-100px] w-[30vw]"
+          src="https://images.bitmoji.com/3d/avatar/765808989-100845834104_4-s5-v1.webp"></img>
         <h1 className="font-bold text-[#777] my-[1rem] md:my-[2rem] w-fit flex items-center gap-[10px]">
           <img src="./account.png" className="w-[30px]"></img>About Me
         </h1>
-        <p className="text-[18px] md:text-[2rem] text-justify">
+        <p className="text-[18px] md:text-[2rem] text-justify md:w-[60%]">
           I am a Web Developer and UI/UX Designer. I have 2+ years of
           experience. My mission is to be best in my skill and give the best
           result from my hardwork and dedication.
@@ -54,11 +96,6 @@ function App() {
         </button>
       </div>
       <div className="m-[2rem] md:mt-[4rem] bg-[#f7f7f7] md:bg-[#fff] rounded-2xl p-[2rem] md:p-0 border-[1px] border-[#bbb] md:border-0 relative">
-        {isMobile && (
-          <span className="absolute flex z-[100] w-[100px] bg-[#604CC3] text-[#fff] text-center rounded-xl p-2 rotate-45 right-[-30px] top-[0]">
-            Skills
-          </span>
-        )}
         <h1 className="font-bold text-[#777] my-[1rem] md:my-[2rem] w-fit">
           Skills I am high ON?
         </h1>
--- src/components/homeCard.jsx
+++ src/components/homeCard.jsx
@@ -1,9 +1,11 @@
-const HomeCard = () => {
+const HomeCard = ({ isVisible }) => {
   return (
     <>
       <span
         data-value="know more about me."
-        className="flex absolute z-[200] cursor-pointer flex-col gap-[12px] md:gap-[3rem] bg-[#f6eaff] p-4 md:p-6 rounded-xl md:rounded-[20px] ring-1 ring-[#bbb] rotate-[-15deg] hover:shadow-xl duration-300 left-0">
+        className={`flex absolute z-[200] cursor-pointer flex-col gap-[12px] md:gap-[3rem] bg-[#f6eaff] p-2 md:p-6 rounded-xl md:rounded-[20px] ring-1 ring-[#bbb] rotate-[-15deg] hover:shadow-xl duration-700 top-[10vh] md:top-[40px] left-0 ${
+          !isVisible ? "opacity-0" : ""
+        }`}>
         <span data-value="know more about me.">
           <img
             className="w-[25px] md:w-[35px]"
@@ -17,17 +19,19 @@ const HomeCard = () => {
         </span>
         <button
           data-value="know more about me."
-          className="bg-[#AC66DD] p-2 md:py-2 md:px-4 text-[10px] md:text-[16px] rounded-md md:rounded-full border-[1px] border-[#822ebe] shadow-xl text-[#fff] active:shadow-none">
+          className="bg-[#AC66DD] w-[120px] md:w-auto p-2 md:py-2 md:px-4 text-[10px] md:text-[16px] rounded-md md:rounded-full border-[1px] border-[#822ebe] shadow-xl text-[#fff] active:shadow-none">
           Wanna know?
         </button>
       </span>
       <span
         data-value="github."
-        className="flex absolute z-[200] flex-col gap-[8rem] md:gap-[9rem] bg-[#eee] p-4 md:p-6 rounded-xl md:rounded-[20px] ring-1 ring-[#bbb] cursor-pointer rotate-[-3deg] hover:shadow-xl duration-300 top-0 right-0 text-black">
+        className={`flex absolute z-[200] flex-col gap-[8rem] md:gap-[9rem] bg-[#eee] p-4 md:p-6 rounded-xl md:rounded-[20px] ring-1 ring-[#bbb] cursor-pointer rotate-[6deg] md:rotate-[-3deg] hover:shadow-xl duration-700 top-[4vh] md:top-[14vh] right-[-100px] text-black ${
+          !isVisible ? "opacity-0" : ""
+        }`}>
         <span data-value="github">
           <img
             className="w-[25px] md:w-[35px]"
-            src="./linkedin.png"
+            src="./github.png"
             data-value="github."></img>
           <p
             className="text-[14px] md:text-[18px] font-medium md:font-semibold text-[#000]"
@@ -40,16 +44,17 @@ const HomeCard = () => {
         </span>
         <button
           data-value="github."
-          className="w-[200px] md:w-[300px] bg-[#02040A] p-2 md:py-2 md:px-4 text-[10px] md:text-[16px] rounded-md md:rounded-full border-[1px] border-[#777] shadow-xl text-[#fff] active:shadow-none flex gap-[10px] md:justify-evenly">
+          className="w-[200px] md:w-[300px] bg-[#02040A] p-2 md:py-2 md:px-4 text-[10px] md:text-[16px] rounded-md border-[1px] border-[#777] shadow-xl text-[#fff] active:shadow-none flex gap-[10px] md:justify-evenly">
           <p data-value="github." className="font-extrabold">
             Follow
           </p>{" "}
-          1.27k
         </button>
       </span>
       <span
         data-value="linkedin."
-        className="flex absolute z-[200] flex-col gap-[2rem] md:gap-[3rem] bg-[#f2f9ff] p-4 md:p-6 rounded-xl md:rounded-[20px] ring-1 ring-[#bbb] cursor-pointer rotate-[9deg] hover:shadow-xl duration-300 top-[50vh] right-0">
+        className={`flex absolute z-[200] flex-col gap-[2rem] md:gap-[3rem] bg-[#f2f9ff] p-4 md:p-6 rounded-xl md:rounded-[20px] ring-1 ring-[#bbb] cursor-pointer rotate-[9deg] hover:shadow-xl duration-700 top-[50vh] right-0 ${
+          !isVisible ? "opacity-0" : ""
+        }`}>
         <span data-value="linkedin">
           <img
             className="w-[25px] md:w-[35px]"
@@ -71,6 +76,48 @@ const HomeCard = () => {
             Follow
           </p>{" "}
           1.27k
+        </button>
+      </span>
+      <span
+        data-value="my pic."
+        className={`flex absolute z-[100] flex-col gap-[2rem] md:gap-[3rem] bg-[#f2f9ff] p-2 md:p-4 rounded-xl md:rounded-[20px] ring-1 ring-[#bbb] cursor-pointer rotate-[5deg] md:rotate-[2deg] hover:shadow-xl duration-700 top-[50vh] md:top-[20vh] left-4 ${
+          !isVisible ? "opacity-0" : ""
+        }`}>
+        <img
+          className="w-[130px] md:w-[300px] rounded-lg"
+          src="./snapRohan.png"
+          data-value="my pic."></img>
+      </span>
+      <span
+        data-value="चरन्मार्गान्विजानाति |"
+        className="flex absolute z-[900] flex-col gap-[2rem] md:gap-[3rem] bg-transparent p-2 rounded-xl md:rounded-[20px] ring-1 ring-[#bbb] cursor-pointer rotate-[5deg] md:rotate-[2deg] hover:shadow-xl duration-700 top-[25vh] md:top-[10vh] left-10 md:left-[20%]">
+        <img
+          data-value="चरन्मार्गान्विजानाति |"
+          className="w-[20px] sm:w-[40px]"
+          src="./heart.png"></img>
+      </span>
+      <span
+        data-value="support."
+        className={`flex w-fit absolute z-[200] flex-col gap-[2rem] md:gap-[3rem] bg-[#fff8c7] p-4 md:p-6 rounded-xl md:rounded-[20px] ring-1 ring-[#bbb] cursor-pointer rotate-[-15deg] md:rotate-[9deg] hover:shadow-xl duration-700 md:top-[55vh] top-[64vh] right-0 md:left-[200px] ${
+          !isVisible ? "opacity-0 md:left-[-100%] md:top-[-100%]" : ""
+        }`}>
+        <span data-value="support">
+          <img
+            className="w-[25px] bg-[#FFDD04] rounded-lg p-[5px] md:w-[35px]"
+            src="./coffee.png"
+            data-value="support."></img>
+          <p
+            className="text-[14px] md:text-[18px] font-medium md:font-semibold text-[#444]"
+            data-value="support.">
+            Support
+          </p>
+        </span>
+        <button
+          data-value="support."
+          className="bg-[#FFDD04] w-[140px] p-2 md:py-2 md:px-4 text-[10px] md:text-[16px] rounded-md border-[1px] border-[#9e8e23] shadow-xl text-[#000] active:shadow-none flex gap-[10px] md:justify-evenly">
+          <p data-value="support." className="font-bold">
+            Support
+          </p>
         </button>
       </span>
     </>
